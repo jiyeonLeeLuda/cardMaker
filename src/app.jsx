53a49b0b36b031d34d5f57527f7503309b14dd41
jiyeonLeeLuda <dlwldusq.work@gmail.com>
2021-11-09 15:14:13 +0900

    라우터 기본 설정

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import './app';
+import Cards from './pages/cards/cards';
+import Login from './pages/login/login';
 
 function App() {
-  return <h1>hello :) </h1>;
+  return (
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/cards' element={<Cards />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/cards/cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cards = (props) => {
+  return (
+    <>
+      <h1>cards</h1>
+      <h5>logout</h5>
+    </>
+  );
+};
+
+export default Cards;
